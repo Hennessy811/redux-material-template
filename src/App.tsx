c1a037dace71aafbb20e4deec4e733e2eb359715
Dmitriy Malakhov <mitia2022@gmail.com>
2021-04-13 13:21:43 +0300

    template setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,47 @@
+import { Container, createMuiTheme, ThemeProvider, useMediaQuery } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
+import { Box } from '@material-ui/core';
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import Notifications from './shared/components/Notifications';
+import useTheme from './shared/hooks/useTheme';
+
+const baseTheme = createMuiTheme();
 
 function App() {
+  const { theme: prefferedTheme } = useTheme();
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = React.useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: (prefferedTheme === 'system' && prefersDarkMode) || prefferedTheme === 'dark' ? 'dark' : 'light',
+        },
+        typography: {
+          h1: {
+            fontSize: baseTheme.typography.pxToRem(42),
+          },
+          h2: {
+            fontSize: baseTheme.typography.pxToRem(36),
+          },
+          h3: {
+            fontSize: baseTheme.typography.pxToRem(28),
+            fontWeight: 100,
+          },
+        },
+      }),
+    [prefersDarkMode, prefferedTheme]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Box>
+        <Container maxWidth="sm">
+          <Typography>Hello world</Typography>
+        </Container>
+        <Notifications />
+      </Box>
+    </ThemeProvider>
   );
 }
 
